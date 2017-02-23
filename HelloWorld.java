a924e605926be95fc5f6a1ddadd4d98a1643040f
ksarabia <sarabia.katherine@yahoo.com>
2017-02-22 21:18:27 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[]args) {
         //Prints out "Hello World." to the terminal window.
 	System.out.println("Hello World.");
     }
